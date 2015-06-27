f71196e75ab4708943de2d14f334661b11361cd0
skepticX <skepticx@hotmail.com>
2015-06-27 11:08:42 +0200

    Updated slightly

--- CS-general_1.js
+++ CS-general_1.js
@@ -1,6 +1,8 @@
 // Find the most frequent integer in an array
+// https://www.reddit.com/r/cscareerquestions/comments/20ahfq/heres_a_pretty_big_list_of_programming_interview/
 (function() {
-var sortedArray = [4, 2, 4, 5, 100, 2, 25, 1, -4, -4, -4, -4, 2, 6].sort(function(a, b){return a-b}); // Numeric sort
+var testArray = [4, 2, 4, 5, 100, 2, 25, 1, -4, -4, -4, -4, 2, 6]
+var sortedArray = testArray.sort(function(a, b){return a - b}); // Numeric sort, ascending
 var count = 0, tempCount = 1, result = 0;
 
 for (var i = 0, x = sortedArray.length; i < x; i++) {
